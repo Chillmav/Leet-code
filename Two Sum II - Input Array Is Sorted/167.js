e4ff74cad876bfa7176206c418a753231d53b559
Kuba <jakub.kulig2004@gmail.com>
2025-04-13 16:48:34 +0200

    more efficient

--- Two Sum II - Input Array Is Sorted/167.js
+++ Two Sum II - Input Array Is Sorted/167.js
@@ -1,14 +1,42 @@
 
-// Brute force O(n^2)
+// Brute force 
+
+// var twoSum = function(numbers, target) {
+
+//     for(let i = 0; i < numbers.length; i++) {
+//         if (numbers[0] + numbers[i] > target) {
+//             numbers.splice(i, numbers.length - 1)
+//         }
+//     }
+    
+//     for(let i = 0; i < numbers.length - 1; i++) {
+//         for (let j = numbers.length - 1; j > i; j--) {
+//             if (numbers[i] + numbers[j] === target) {
+//                 return [i + 1, j + 1]
+
+//             }
+//         }
+//     }
+// };
+
+// Efficient solution
 
 var twoSum = function(numbers, target) {
+    let result = false
+    let i = 0;
+    let j = numbers.length - 1
 
-    for(let i = 0; i < numbers.length - 1; i++) {
-        for (let j = numbers.length - 1; j > i; j--) {
-            if (numbers[i] + numbers[j] === target) {
-                return [i + 1, j + 1]
+    while (!result) {
 
-            }
+        if (numbers[i] + numbers[j] === target) {
+
+            return [i + 1, j + 1]
+
+        } else if (numbers[i] + numbers[j] > target) {
+            j--
+        } else {
+            i++
         }
     }
+    
 };
