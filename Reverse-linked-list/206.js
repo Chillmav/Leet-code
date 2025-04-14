74af41bf67608a3da64482db78ac244bc9d1f5ef
Kuba <jakub.kulig2004@gmail.com>
2025-04-14 20:09:30 +0200

    iteratively

---
+++ Reverse-linked-list/206.js
@@ -0,0 +1,11 @@
+var reverseList = function(head) {
+    let prev = null;
+    let curr = head;
+    while (curr) {
+        const nxt = curr.next
+        curr.next = prev
+        prev = curr
+        curr = nxt
+    }
+    return prev
+};
