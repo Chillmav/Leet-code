e1852b45d7e36ca0a67b8f852671b4b85c5ae85a
Kuba <jakub.kulig2004@gmail.com>
2025-04-17 13:50:51 +0200

    O(n)

--- Product-of-array-except-self.js
+++ Product-of-array-except-self.js
@@ -31,3 +31,31 @@ var productExceptSelf = function(nums) {
 
 */
 
+var productExceptSelf = function(nums) {
+    const prefix = [];
+    const postfix = [];
+
+    // building prefix
+    for (let i = 0; i < nums.length; i++) {
+        if (i === 0) {
+            prefix.push(1)
+        } else {
+            prefix.push(nums[i - 1] * prefix[i - 1])
+        }
+    }
+
+    // building postfix
+    for (let i = nums.length - 1; i >= 0; i--) {
+        if (i === nums.length - 1) {
+            postfix[i] = 1
+        } else {
+            postfix[i] = nums[i + 1] * postfix[i + 1]
+        }
+    }
+
+    nums = nums.map((val, index) => {
+        return prefix[index] * postfix[index]
+    })
+    return nums
+};
+
