375e36ac753f1db93b0ab95c3d3cbe0b64dca5cb
Kuba <jakub.kulig2004@gmail.com>
2025-04-18 23:04:23 +0200

    stack version

--- Valid-Parentheses.js
+++ Valid-Parentheses.js
@@ -1,3 +1,4 @@
+/* Brute Force
 var isValid = function(s) {
     while (s.includes('()') || s.includes('{}') || s.includes('[]')) {
         s = s.replace('()', '');
@@ -6,3 +7,37 @@ var isValid = function(s) {
     }
     return s === ''
 };
+*/
+
+/* stack
+var isValid = function(s) {
+    const stack = [];
+
+    const validation = {
+
+        "}": '{',
+        ']': '[',
+        ')': '('
+    };
+
+    while (s.length) {
+
+        const element = s.charAt(0);
+        s = s.slice(1);
+
+        if (element in validation) {
+            if (stack.length === 0 || stack[stack.length - 1] !== validation[element]) {
+                return false
+            }
+            stack.pop()
+        } else {
+
+            stack.push(element)
+
+        }
+
+    }
+    return stack.length === 0
+};
+
+*/
