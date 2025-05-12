00156f2cb017ccf5fd7722853f60e4917b5c68f7
Kuba <jakub.kulig2004@gmail.com>
2025-05-12 20:40:45 +0200

    linked-lists

---
+++ text.java/CarFleet.java
@@ -0,0 +1,14 @@
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class CarFleet {
+
+    public static void main(String[] args) {
+
+        int [] arr = {1,3,6,2,0};
+        Arrays.sort(arr);
+
+        System.out.println(Arrays.toString(arr));
+    }
+}
