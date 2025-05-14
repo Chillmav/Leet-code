f86675ac8c9e25b34cf72079085ea1c970e0f918
Kuba <jakub.kulig2004@gmail.com>
2025-05-14 23:10:24 +0200

    linky cycle

---
+++ Linked-Lists/LinkedListCycle.java
@@ -0,0 +1,19 @@
+
+public class LinkedListCycle {
+    public boolean hasCycle(ListNode head) {
+
+        ListNode slower = head;
+        ListNode faster = head;
+
+        while (faster != null && faster.next != null) {
+            slower = slower.next;
+            faster = faster.next.next;
+
+            if (slower == faster) {
+                return true;
+            } 
+        }
+
+        return false;
+    }
+}
